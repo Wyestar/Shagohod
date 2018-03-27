1a67894211e377da89a50a5284075ed71e94f9e9
Wyestar <food.bolus@gmail.com>
2018-03-27 17:18:55 -0400

    additional prefix

--- app.js
+++ app.js
@@ -13,28 +13,38 @@ client.on("message", async message => {
 
   if(message.author.bot) return;
 
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  // if(message.content.indexOf(config.ping_prefix) !== 0) return;
 
-  // Here we separate our "command" name, and our "arguments" for the command.
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+  if(message.content.indexOf(config.ping_prefix) === 0) {
+    // Here we separate our "command" name, and our "arguments" for the command.
+    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+    // command = say
+    // args = ["Is", "this", "the", "real", "life?"]
+    const args = message.content.slice(config.ping_prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-  if(command === "jeff" || command === "jef" || command === "geff" || command === "gef" || command === "geoff" || command === "jeffrey" || command === "gev") {
-    const display_name = message.member.nickname;
+    if(command === "jeff" || command === "jef" || command === "geff" || command === "gef" || command === "geoff" || command === "jeffrey" || command === "gev") {
+      const display_name = message.member.nickname;
 
-    const yo_jeff = display_name + " says: <@96084289279500288> " + args.join(" ");
+      const yo_jeff = display_name + " says: <@96084289279500288> " + args.join(" ");
 
-    // message.delete().catch(^_^=>{});
-    // laksjflaks
-    message.channel.send(yo_jeff);
+      // message.delete().catch(^_^=>{});
+      // laksjflaks
+      message.channel.send(yo_jeff);
+    }
+    else if(command === "iroquoispliskin") {
+      message.delete(3000).catch(()=>{});
+      message.channel.send("Who's that? They must be friends with Quattro Bajeena...")
+    }
   }
 
-  if(command === "iroquoispliskin") {
-    message.delete(3000).catch(()=>{});
-    message.channel.send("Who's that? They must be friends with Quattro Bajeena...")
+  else if(message.content.indexOf(config.cmd_prefix) === 0) {
+    const args = message.content.slice(config.ping_prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command === "info" || command === "help" || command === "commands") {
+      message.channel.send("go home and be a family man");
+    }
   }
 
   // if(command === "sunshine") {
@@ -46,6 +56,8 @@ client.on("message", async message => {
   //   // attachment is path to file location
   //   Client.sendFile(message.channel, attachment)
   // }
+
+  return;
 });
 
 client.login(config.token);
