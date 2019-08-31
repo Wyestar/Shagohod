f1c70d297f9a989c7a7da4dd495cfced80c3cbca
Douglas Chin <food.bolus@gmail.com>
2019-08-31 14:18:57 -0400

    token update
    git ignore fixed

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require("./config.json");
 
 client.on("ready", () => {
-  const games = [`Metal Gear Survive 2: Lords of Dust`, 'Silent Hillz', 'Metal Gear Rising Deux: Les Enfants des Épée (Lightning Bolt Action)', 'Metal Gear: Ghost Babel', 'Metal Gear Solid 6: Widow of Cipher', 'Zone of the Enders III: Shadow of Callisto', 'Snatcher 3: Bioroids Divided', 'Castlevania Judgement 2', 'Pro Evolution Soccer 2004', 'International Superstar Soccer 2', 'Death Stranding II: Genesis Nova'];
-  const random = Math.floor(Math.random() * 11);
+  const games = [`Metal Gear Survive 2: Lords of Dust`, 'Silent Hillz', 'Metal Gear Rising Deux: Les Louves des Épée', 'Metal Gear: Ghost Babel', 'Metal Gear Solid 6: Widow of Cipher', 'Zone of the Enders III: Shadow of Callisto', 'Snatcher 3: Bioroids Divided', 'Castlevania Judgement 2', 'Pro Evolution Soccer 2004',  'Death Stranding II: Genesis Nova'];
+  const random = Math.floor(Math.random() * 10);
   client.user.setGame(games[random]);
 });
 
 client.on("message", async message => {
-  // searches each message for prefixes at beginning
 
   if(message.author.bot) return;
 
   const args = message.content.slice(1).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if(message.content.indexOf(config.ping_prefix) === 0) {
+  if(message.content.indexOf(config.pingPrefix) === 0) {
     if(command === "jeff" || command === "jef" || command === "geff" || command === "gef" || command === "geoff" || command === "jeffrey" || command === "gev" || command === "gef") {
       const display_name = message.member.nickname;
-      const yo_jeff = display_name + " says: <@96084289279500288> " + args.join(" ");
-      message.channel.send(yo_jeff);
+      const yoJeff = display_name + " says: <@96084289279500288> " + args.join(" ");
+      message.channel.send(yoJeff);
     }
     else if(command === "iroquoispliskin") {
       message.delete(4000).catch(()=>{});
@@ -29,14 +28,14 @@ client.on("message", async message => {
     }
   }
 
-  else if(message.content.indexOf(config.cmd_prefix) === 0) {
+  else if(message.content.indexOf(config.cmdPrefix) === 0) {
     if(command === "info" || command === "help" || command === "commands") {
-      message.channel.send("Go home and be a family man.");
+      message.channel.send("Go home and brush your teeth.");
     }
   }
 
   // shop feature
-  else if(message.content.indexOf(config.fortune_prefix) === 0) {
+  else if(message.content.indexOf(config.fortunePrefix) === 0) {
 
     if(command === "sunny" || command === "eggs") {
       message.channel.send("https://static.giantbomb.com/uploads/original/0/2020/941695-2573736726_af67ec3b18_o.jpg")
