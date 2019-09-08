85d1c99c3a74713ece7df1229d4d7e379b087207
Douglas Chin <food.bolus@gmail.com>
2019-09-08 18:22:39 -0400

    config updates

--- messageUtilities/fortunes/fortune.js
+++ messageUtilities/fortunes/fortune.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+// const axios = require('axios');
 // axios.defaults.baseURL = 'https://www.googleapis.com';
 // axios.defaults.headers.common['Authorization'] = '251300381219';
 // axios.defaults.headers.post['Content-Type'] = 'application/json';
@@ -6,12 +6,12 @@ const axios = require('axios');
 const fortuneStatic = require("./fortuneStatic.json");
 const fortuneCloud = require("./fortuneCloud.json");
 
-const getFortunesFromS3 = () => {
-	return axios.get('https://shagohod-fortune.s3.us-east-2.amazonaws.com/fortuneCloud.json').then(response => {
-		console.error('res data111: ', response.data)
-		return response.data
-	})
-}
+// const getFortunesFromS3 = () => {
+// 	return axios.get('https://shagohod-fortune.s3.us-east-2.amazonaws.com/fortuneCloud.json').then(response => {
+// 		// console.error('res data111: ', response.data)
+// 		return response.data
+// 	})
+// }
 
 	// let data = null;
 	// try {
@@ -33,20 +33,22 @@ const getFortunesFromS3 = () => {
 // 	return fortunes;
 // };
 
-const fortune = async (message) => {
+const fortune = (message) => {
 	const args = message.content.slice(1).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
 	if (command === "collude") {
 		if (message.author.id === "96084289279500288") {
-			let test = null;
-			await getFortunesFromS3().then(data => {
-				console.error('data222: ', data);
-				test = data;
-			});
-			console.error('s3 test final: ', test);
-			const amount = fortuneCloud["96084289279500288"];
-			return "Collusion successful; Gev now has " + amount + " Drebin Points";
+			// let test = null;
+			// await getFortunesFromS3().then(data => {
+			// 	// console.error('data222: ', data);
+			// 	test = data;
+			// });
+			// console.error('s3 test final: ', test);
+			const amount = fortuneCloud["96084289279500288"] + 10;
+			// console.error('s3 amount final: ', amount);
+
+			return "Collusion successful; Gev now has "+amount+" Drebin Points";
 		}
 
 		return "You can not collude."
